bee5d57030559e1dfdb7afd99345ba6173f1d508
SagertStav <135542866+SagertStav@users.noreply.github.com>
2023-06-10 23:07:37 +0300

    Update str_func.py (добавил докстринги)
    Внесены докстринги модуля и функции

--- str_func.py
+++ str_func.py
@@ -1,4 +1,7 @@
+""" Модуль описания функций. Работа со строками """
+
 def hi_reg_str(any_string):
+""" Ф-ция hi_reg_str(any_string) возвращает все буквы ПРОПИСНЫМИ """    
     return any_string.upper()
 
 #print(hi_reg_str("drova"))
